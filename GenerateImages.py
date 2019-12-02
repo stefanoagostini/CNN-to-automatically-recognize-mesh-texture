ca93d22ca6fa693ea63d17e2ab8b0aa87d2bb56e
ScaramuzzinoGiovanna <Scaramuzzinogiovanna@live.it>
2019-12-02 10:40:55 +0100

    rescale pixel

--- GenerateImages.py
+++ GenerateImages.py
@@ -15,6 +15,7 @@ def sortFour(val):
     """
     return val[3]
 
+
 def createFolder(path, name):
     """
     goal:create new folder
@@ -36,13 +37,42 @@ def createFolder(path, name):
     return path_folder
 
 
+def min_max_pixel(train):
+    """
+      goal:determinate minimum and maximum of pixels
+
+      :param train: vector containing path with nwe name of images
+      :return: values minimum and maximum of total pixels
+      """
+    for i, elem in enumerate(train):
+        mat = sio.loadmat(elem[0])
+        if i == 0:
+            minimum = np.min(mat['grid_data'][0][0][1])
+            maximum = np.max(mat['grid_data'][0][0][1])
+        else:
+            if minimum > np.min(mat['grid_data'][0][0][1]):
+                minimum = np.min(mat['grid_data'][0][0][1])
+            if minimum > np.min(mat['grid_data'][0][0][2]):
+                minimum = np.min(mat['grid_data'][0][0][2])
+            if minimum > np.min(mat['grid_data'][0][0][3]):
+                minimum = np.min(mat['grid_data'][0][0][3])
+            if maximum < np.max(mat['grid_data'][0][0][1]):
+                maximum = np.max(mat['grid_data'][0][0][1])
+            if maximum < np.max(mat['grid_data'][0][0][2]):
+                maximum = np.max(mat['grid_data'][0][0][2])
+            if maximum < np.max(mat['grid_data'][0][0][3]):
+                maximum = np.max(mat['grid_data'][0][0][3])
+            else:
+                pass
+    return minimum, maximum
+
+
 def arrayToImages(path_files, path_label, path_f):
     """
     Goal: create an array containing 3 channels characterized by descriptor: localdepth, azimuth,elevation
 
-    :param path_files: data path
-    :param path_label: label path
-    :param path_f: path into save images
+    :param vect: vector
+    :param path_f: path into save image
     """
     print('[INFO] generate and save images...')
     train = []
@@ -60,6 +90,10 @@ def arrayToImages(path_files, path_label, path_f):
         tmp_train.sort(key=sortFour)
         train = train + tmp_train
 
+    minimum, maximum = min_max_pixel(train)
+    print(minimum, maximum)
+    old_range = maximum - minimum
+    new_range = 255
     for elem in train:
         mat = sio.loadmat(elem[0])
         facet = elem[1][0][0]
@@ -67,14 +101,17 @@ def arrayToImages(path_files, path_label, path_f):
         ori = elem[4]
         label = elem[5][0][0][0]
 
-        localdepth = (mat['grid_data'][0][0][1]) * 100
-        azimuth = (mat['grid_data'][0][0][2]) * 100
-        elevation = (mat['grid_data'][0][0][3]) * 100
+        scale_l = ((mat['grid_data'][0][0][1]) - minimum) / old_range
+        localdepth = (new_range * scale_l)
+        scale_a = ((mat['grid_data'][0][0][2]) - minimum) / old_range
+        azimuth = (new_range * scale_a)
+        scale_e = ((mat['grid_data'][0][0][3]) - minimum) / old_range
+        elevation = (new_range * scale_e)
 
         vect_img = np.array([localdepth, azimuth, elevation])
         vect_img = vect_img.transpose(1, 2, 0)
 
-        facet = np.array2string(facet)  # string
+        facet = np.array2string(facet)
         ori = str(ori)
         label = str(label)
         tmp_path_f = path_f + "/" + label
@@ -85,13 +122,12 @@ def saveImages(path_img, vect_img):
     """
     Goal: create image end save this
 
-    :param path_img: path also containing the name of image, into save the image
+    :param path: path also containing the name of image, into save the image
     :param vect_img: array from which to create the image
 
     """
 
     cv2.imwrite(path_img + '.png', vect_img)
-
 
 
 path_files = 'Data/Dataset_grids/'
